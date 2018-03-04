b819384d974cd300c054c3e5c46f272ccd72a25a
James MacIsaac <james@jmproj.com>
2018-03-04 16:00:39 -0700

    Added skeleton files to prepare for planning out the project structure

---
+++ GAARLS/src/Input/InputManager.java
@@ -0,0 +1,16 @@
+/*
+ * Authors: 	Peter Ebear, David Engel, Evan Loughlin, James MacIsaac, Shane Sims
+ * Project: 	Genetic Algorithm based Association Rule Learning System
+ * Course: 	CPSC 599.44 - Machine Learning - Winter 2018
+ * Professor:	Dr. Jörg Denzinger
+ * University:	University of Calgary
+ */
+package Input;
+
+/**
+ *
+ * @author Peter Ebear, David Engel, Evan Loughlin, James MacIsaac, Shane Sims
+ */
+public class InputManager {
+    // This is a skeleton for the Managing class of all input related code
+}
---
+++ GAARLS/test/InputTests/BasicInputTest.java
@@ -0,0 +1,47 @@
+/*
+ * Authors: 	Peter Ebear, David Engel, Evan Loughlin, James MacIsaac, Shane Sims
+ * Project: 	Genetic Algorithm based Association Rule Learning System
+ * Course: 	CPSC 599.44 - Machine Learning - Winter 2018
+ * Professor:	Dr. Jörg Denzinger
+ * University:	University of Calgary
+ */
+package InputTests;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Peter Ebear, David Engel, Evan Loughlin, James MacIsaac, Shane Sims
+ */
+public class BasicInputTest {
+    
+    public BasicInputTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+}
