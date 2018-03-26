93008f510bf3e76c75cef6ffed95b6cae8f11709
James MacIsaac <james@jmproj.com>
2018-03-26 17:48:43 -0600

    Added the Config file logic to the Parser class along with a configuration wrapper class so that it can persist. Updated main to display config info

--- GAARLS/src/Main.java
+++ GAARLS/src/Main.java
@@ -12,6 +12,7 @@ public class Main
     private static String dataFilePath = "NCDB_1999_to_2015.csv";
     private static String ruleFilePath = "rules.txt";
     private static String wekaFilePath = "weka.txt";
+    private static String configFilePath = "config.txt";
 
     public static void main(String args[])
     {
@@ -32,6 +33,11 @@ public class Main
         Parser parser = new Parser();
         ArrayList<Rule> knownRules = parser.parseKnownRules(ruleFilePath, featuresToOmit);
         ArrayList<Rule> wekaRules = parser.parseWekaRules(wekaFilePath, lookupTable, featuresToOmit);
+        
+        //new wrapper class containing the config parameter values read from file
+        ConfigParameters cp = parser.parseConfigParameters(configFilePath);
+        printConfigDetails(cp); // I assume this will help a lot
+        //TODO: need to decide who is going to link these options up to the proper locations
 
         EvolutionManager evolutionManager = new EvolutionManager(database, lookupTable, knownRules, 10);
         evolutionManager.evolve(1000, 1000, 1300);
@@ -43,7 +49,8 @@ public class Main
      *     -d <datafile>
      *     -r <rulefile>
      *     -w <wekafile>
-     * ex: java main.java -d data.csv -w weka.txt -r rules.txt
+     *     -c <configfile>
+     * ex: java main.java -d data.csv -w weka.txt -r rules.txt -c config.txt
      * use these switches so we don't rely on ordering
      * in case we want any combination of these files for our personal tests
      * e.g. I don't forsee myself always using a rulefile and wekafile
@@ -51,19 +58,60 @@ public class Main
     private static void parseArgs(String args[]) {
         try {
             for (int i = 0; i < args.length; ++i) {
-                if (args[i].equals("-d")) {
-                    dataFilePath = args[++i];
-                } else if (args[i].equals("-r")) {
-                    ruleFilePath = args[++i];
-                } else if (args[i].equals("-w")) {
-                    wekaFilePath = args[++i];
-                } else if (args[i].equals("-l")) {
-                    lookupFilePath = args[++i];
+                switch (args[i]) {
+                    case "-d":
+                        dataFilePath = args[++i];
+                        break;
+                    case "-r":
+                        ruleFilePath = args[++i];
+                        break;
+                    case "-w":
+                        wekaFilePath = args[++i];
+                        break;
+                    case "-l":
+                        lookupFilePath = args[++i];
+                        break;
+                    case "-c":
+                        configFilePath = args[++i];
+                        break;
+                    default:
+                        break;
                 }
             }
         } catch (ArrayIndexOutOfBoundsException e) {
             System.out.println("ERROR: Malformed commandline: " + Arrays.toString(args));
             System.out.println("Available optional switches: \n\t-d <datafile> \n\t-r <rulefile> \n\t-w <wekafile> \n\t-l <lookupfile>");
         }
+    }
+    
+    /** printConfigDetails: method to quickly print the configuration details 
+     * for this run of the program
+     * 
+     * @param cp - the ConfigParameters object to display info about
+     * 
+     * this should be handy if and when we get into running a series of 
+     * consecutive configurations on our system to evaluate a personal experiment
+     */
+    private static void printConfigDetails(ConfigParameters cp){
+        System.out.println("---Configuration parameters---");
+        System.out.println("Initial Population Size: \t\t"+cp.initialPopSize);
+        System.out.println("Number Of Generations: \t\t\t"+cp.numGenerations);
+        System.out.println("Maximum Population Size: \t\t"+cp.populationMax);
+        System.out.println("Min Coverage: \t\t\t\t"+cp.minCoverage);
+        System.out.println("Min Accuracy: \t\t\t\t"+cp.minAccuracy);
+        System.out.println("Probability of Crossover: \t\t"+cp.probOfCrossover);
+        System.out.println("Probability of Mutation: \t\t"+cp.probOfMutation);
+        System.out.println("Base Fitness Weighting: \t\t"+cp.baseFitnessWeight);
+        System.out.println("Ext1 Fitness Weighting: \t\t"+cp.ext1FitnessWeight);
+        System.out.println("Ext2 Fitness Weighting: \t\t"+cp.ext2FitnessWeight);
+        System.out.println("Num Antecedent Features: \t\t"+cp.numFeatAntecedent);
+        System.out.println("Num Consequent Features: \t\t"+cp.numFeatConsequent);
+        System.out.print("Indices of Features to Ignore: \t\t");
+        System.out.print("[");
+        cp.featuresToIgnore.forEach((feature) -> {
+            System.out.print(" "+feature+" ");
+        });
+        System.out.println("]");
+        System.out.println("------------------------------");
     }
 }
--- GAARLS/src/Parser.java
+++ GAARLS/src/Parser.java
@@ -132,4 +132,120 @@ public class Parser {
         }*/
         return wekaRules;
     }
+    
+    public ConfigParameters parseConfigParameters(String configFilePath) {
+        File configFile = new File(configFilePath);
+        if (configFile.exists()) {
+            System.out.println("Found config file '" + configFilePath + "'. Parsing configuration parameters...");
+            try {
+                BufferedReader br = new BufferedReader(new FileReader(configFile));
+                String paramLine;
+                // pull out the relevant portions of the rule
+
+                Pattern emptyLinePatt = Pattern.compile("^\\s*$");
+                Pattern initPopPatt = Pattern.compile("^INITIAL_POP_SIZE\\s*=\\s*\\d*$");
+                Pattern numGenPatt = Pattern.compile("^NUM_GENERATIONS\\s*=\\s*\\d*$");
+                Pattern popMaxPatt = Pattern.compile("^MAX_POPULATION\\s*=\\s*\\d*$");
+
+                Pattern minCovPatt = Pattern.compile("^MIN_COVERAGE\\s*=\\s*\\d*(.\\d*)?$");
+                Pattern minAccPatt = Pattern.compile("^MIN_ACCURACY\\s*=\\s*\\d*(.\\d*)?$");
+
+                Pattern pXPatt = Pattern.compile("^P_OF_CROSSOVER\\s*=\\s*\\d*(.\\d*)?$");
+                Pattern pMPatt = Pattern.compile("^P_OF_MUTATION\\s*=\\s*\\d*(.\\d*)?$");
+
+                Pattern bFWPatt = Pattern.compile("^BASE_FITNESS_WEIGHT\\s*=\\s*\\d*(.\\d*)?$");
+                Pattern e1FWPatt = Pattern.compile("^EXT1_FITNESS_WEIGHT\\s*=\\s*\\d*(.\\d*)?$");
+                Pattern e2FWPatt = Pattern.compile("^EXT2_FITNESS_WEIGHT\\s*=\\s*\\d*(.\\d*)?$");
+
+                Pattern numFAPatt = Pattern.compile("^NUM_FEATURES_ANTE\\s*=\\s*\\d*$");
+                Pattern numFCPatt = Pattern.compile("^NUM_FEATURES_CONS\\s*=\\s*\\d*$");
+                Pattern featTIPatt = Pattern.compile("^FEATURES_TO_IGNORE\\s*=\\s*\\d(\\s*,\\s*\\d)*$");
+
+                Integer initialPopSize = null, numGenerations = null, populationMax = null;
+                Float minCoverage = null, minAccuracy = null;
+                Float probOfCrossover = null, probOfMutation = null;
+                Float baseFitnessWeight = null, ext1FitnessWeight = null, ext2FitnessWeight = null;
+                Integer numFeatAntecedent = null, numFeatConsequent = null;
+                ArrayList<Integer> featuresToIgnore = null;
+
+                while ((paramLine = br.readLine()) != null) {
+                    if (paramLine.contains("//")) {
+                        paramLine = paramLine.split("//")[0]; // remove comments from line
+                    }
+                    paramLine = paramLine.trim();
+                    paramLine = paramLine.toUpperCase();
+                    try {
+                        if (emptyLinePatt.matcher(paramLine).find()) {
+                            continue; // nothing to due for empties
+                        } else if (initPopPatt.matcher(paramLine).find()) {
+                            initialPopSize = Integer.parseInt(paramLine.split("=")[1].trim());
+                            continue;
+                        } else if (numGenPatt.matcher(paramLine).find()) {
+                            numGenerations = Integer.parseInt(paramLine.split("=")[1].trim());
+                            continue;
+                        } else if (popMaxPatt.matcher(paramLine).find()) {
+                            populationMax = Integer.parseInt(paramLine.split("=")[1].trim());
+                            continue;
+                        } else if (minCovPatt.matcher(paramLine).find()) {
+                            minCoverage = Float.parseFloat(paramLine.split("=")[1].trim());
+                            continue;
+                        } else if (minAccPatt.matcher(paramLine).find()) {
+                            minAccuracy = Float.parseFloat(paramLine.split("=")[1].trim());
+                            continue;
+                        } else if (pXPatt.matcher(paramLine).find()) {
+                            probOfCrossover = Float.parseFloat(paramLine.split("=")[1].trim());
+                            continue;
+                        } else if (pMPatt.matcher(paramLine).find()) {
+                            probOfMutation = Float.parseFloat(paramLine.split("=")[1].trim());
+                        } else if (bFWPatt.matcher(paramLine).find()) {
+                            baseFitnessWeight = Float.parseFloat(paramLine.split("=")[1].trim());
+                        } else if (e1FWPatt.matcher(paramLine).find()) {
+                            ext1FitnessWeight = Float.parseFloat(paramLine.split("=")[1].trim());
+                        } else if (e2FWPatt.matcher(paramLine).find()) {
+                            ext2FitnessWeight = Float.parseFloat(paramLine.split("=")[1].trim());
+                        } else if (numFAPatt.matcher(paramLine).find()) {
+                            numFeatAntecedent = Integer.parseInt(paramLine.split("=")[1].trim());
+                        } else if (numFCPatt.matcher(paramLine).find()) {
+                            numFeatConsequent = Integer.parseInt(paramLine.split("=")[1].trim());
+                        } else if (featTIPatt.matcher(paramLine).find()) {
+                            String[] feats = paramLine.split("=")[1].trim().split(",");
+                            ArrayList<Integer> featList = new ArrayList<>();
+                            for (String feat : feats) {
+                                featList.add(Integer.parseInt(feat.trim()));
+                            }
+                            if (!featList.isEmpty()) {
+                                featuresToIgnore = featList;
+                            }
+                        }
+                    } catch (Exception e) {
+                        System.out.println("ERROR: There was an issue parsing [" + paramLine + "]");
+                    }
+
+                }
+                //TODO: lets talk about these default parameters and set them appropriately
+                ConfigParameters cp = new ConfigParameters(
+                        initialPopSize != null ? initialPopSize : 100, 
+                        numGenerations != null ? numGenerations : 1000,
+                        populationMax != null ? populationMax : 1000, 
+                        minCoverage != null ? minCoverage : 0.01f, 
+                        minAccuracy != null ? minAccuracy : 0.01f, 
+                        probOfCrossover != null ? probOfCrossover : 0.85f, 
+                        probOfMutation != null ? probOfMutation : 0.15f, 
+                        baseFitnessWeight != null ? baseFitnessWeight : 1.0f,
+                        ext1FitnessWeight != null ? ext1FitnessWeight : 1.0f, 
+                        ext2FitnessWeight != null ? ext2FitnessWeight : 1.0f,
+                        numFeatAntecedent != null ? numFeatAntecedent : 2, 
+                        numFeatConsequent != null ? numFeatConsequent : 2,
+                        featuresToIgnore != null ? featuresToIgnore : null
+                );
+                return cp; // return the new obj
+            } catch (IOException io) {
+                System.out.println("ERROR: I/O error when parsing rule file '" + configFilePath + "'");
+                System.out.println(io.getMessage());
+                return null; // had an issue
+            }
+        }
+        return null; // couldnt find file
+    }
+    
 }
